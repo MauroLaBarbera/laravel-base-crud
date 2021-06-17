4a7ec5fb87a3952801c910686ee3a156f339052a
MauroLaBarbera <you@example.com>
2021-06-17 15:45:09 +0200

    fix

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,12 +6,14 @@
         <div class="row">
             <div class="col-md-2">
                 <img class="img-fluids" src="{{ $comics->thumb}}" alt="{{ $comics->title }}">
+                <h5 class="btn btn-primary mt-2">Series: {{ $comics->series}}</h5>
+                <h5 class="btn btn-warning">Sale Date: {{ $comics->sale_date}}</h5>
             </div>
             <div class="col-md-8">
                  <p>{{ $comics->description }}</p>
             </div>
         </div>
-        <h2>Price: {{ $comics->price}}€</h2>
+        <h2>Price: {{ $comics->price}}$</h2>
        <a href="{{ route('comics.index') }}"> Back to Archive</a>
     </div>
 @endsection
